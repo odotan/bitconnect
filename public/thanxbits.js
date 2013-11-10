dc8b647a25f2caee592b9a3916ec67b0f1822a74
vub <v@buterin.com>
2013-11-11 02:07:13 +0300

    Made login and account creation screen work

--- public/thanxbits.js
+++ public/thanxbits.js
@@ -3,24 +3,45 @@ angular.module('thanxbits', []);
 var el = function(x) { return document.getElementById(x); }
 var qs = function(x) { return document.querySelectorAll(x); }
 
+var errhandle = function(r) { console.log("Error",r); }
+
 function ThanxCtrl($scope, $http, $location) {
     window.wscope = $scope;
+
+    $scope.checkname = function() {
+        $http.post('/checkname',{ name:$scope.firstname+'.'+$scope.lastname })
+            .success(function(r) {
+                if (r == '"available"') $scope.available = true;
+                else $scope.available = false;
+             })
+            .error(errhandle);
+    }
+    $scope.register = function() {
+        $http.post('/register',{ name:$scope.firstname+'.'+$scope.lastname })
+            .success(function(u) {
+                $scope.user = u;
+             })
+            .error(errhandle);
+    }
+    $scope.kill = function() {
+        $http.post('/kill')
+            .success(function(r) {
+                $scope.user = r 
+                $scope.checkname()
+             })
+            .error(errhandle);
+    }
 
     $scope.$watch('firstname',$scope.checkname)
     $scope.$watch('lastname',$scope.checkname)
 
-    $scope.checkname = function() {
-        $http.post('/checkname',{name:'$scope.firstname+'.'+$scope.lastname'})
-            .success(function(r) {
-                if (r == 'available') $scope.available = true;
-                else $scope.available = false;
-            });
-    }
-    $scope.register = function() {
-        $http.post('/register',{name:'$scope.firstname+'.'+$scope.lastname'})
-            .success(function(u) {
-                $scope.user = u;
-            });
-    }
+    $http.get('/me')
+        .success(function(u) { 
+            $scope.user = u
+            $scope.firstname = $scope.user.fbUser.first_name.toLowerCase();
+            $scope.lastname = $scope.user.fbUser.last_name.toLowerCase();
+         })
+        .error(errhandle);
 
 }
+
--- server.js
+++ server.js
@@ -31,7 +31,8 @@ app.configure(function() {
      app.set('views',__dirname + '/views'); 
      app.set('view engine', 'jade'); app.set('view options', { layout: false });
      app.use(express.bodyParser());
-     app.use(express.methodOverride());
+     app.use(express.cookieParser());
+     app.use(express.session({ secret: '314159265358979' }));
      app.use(Facebook.middleware({ appId: FBappId, secret: FBsecret }));
      app.use(app.router);
      app.use(express.static(__dirname + '/public'));
@@ -46,24 +47,56 @@ app.use('/',function(req,res) {
 });
 
 app.get('/login', Facebook.loginRequired(), function (req, res) {
-    req.facebook.api('/me', mkrespcb(res,400,function(err, user) {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-            res.end('Hello, ' + user.name + '!');
+    req.facebook.api('/me', mkrespcb(res,400,function(profile) {
+        res.render('app.jade',{});
     }));
 });
 
-app.get('/checkname', function(req,res) {
-    db.User.find({ username: req.param('name') },mkrespcb(res,400,function(u) {
+app.post('/kill', Facebook.loginRequired(), function(req, res) {
+    req.facebook.api('/me', mkrespcb(res,400,function(profile) {
+        db.User.findOne({ id: profile.id },mkrespcb(res,400,function(u) {
+            if (!u) return res.json("User not found",400); 
+            db.User.remove({ id: profile.id },mkrespcb(res,400,function() {
+                res.json({ username: null, fbUser: profile });
+            }));
+        }));
+    }));
+});
+
+app.get('/terms',function(req,res) {
+    res.render('terms.jade',{}) 
+});
+
+app.get('/me', Facebook.loginRequired(), function(req,res) {
+    req.facebook.api('/me', mkrespcb(res,400,function(profile) { 
+        db.User.findOne({ id: profile.id },mkrespcb(res,400,function(u) {
+            if (u) res.json(u);
+            else res.json({ username: null, fbUser: profile });
+        }));
+    }));
+});
+
+app.post('/checkname', function(req,res) {
+    db.User.findOne({ username: req.param('name') },mkrespcb(res,400,function(u) {
         if (!u) res.json('available');
         else res.json('used');
     }));
 });
 
-app.get('/register', Facebook.loginRequired(), function(req, res) {
-    req.facebook.api('/me', mkrespcb(res,400,function(err, user) {
-        db.User.find({ username: req.param('name') },mkrespcb(res,400,function(u) {
+app.post('/register', Facebook.loginRequired(), function(req, res) {
+    req.facebook.api('/me', mkrespcb(res,400,function(profile) {
+        db.User.findOne({ username: req.param('name') }, mkrespcb(res,400,function(u) {
             if (u) return res.json('Account already exists',400);
-            db.User.create({ username: req.param('name'), fbUser: user, txn: 5432 })
+            var newuser = { 
+                username: req.param('name'),    
+                fbUser: profile,
+                id: profile.id,
+                tnx: 5432 
+            }
+            req.param(newuser);
+            db.User.insert(newuser,mkrespcb(res,400,function() {
+                res.json(newuser);
+            }));
         }));
     }));
 });
@@ -71,3 +104,4 @@ app.get('/register', Facebook.loginRequired(), function(req, res) {
 app.listen(3191);
 
 return app;
+
