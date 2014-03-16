a6c549ffcf543fd9e1b4b34d3f4c99c371eef45e
johnnyaug <johnnyaug@gmail.com>
2014-03-17 00:53:39 +0200

    polling performance improvment

--- public/js/services/me.js
+++ public/js/services/me.js
@@ -5,10 +5,12 @@ window.app.service('me', function($rootScope, $http) {
 	this.getme = function() {
 		$http.get('/me')
 			.success(function(u) {
-				$rootScope.user = u;
-				$rootScope.firstname = $rootScope.user.fbUser.first_name.toLowerCase();
-				$rootScope.lastname = $rootScope.user.fbUser.last_name.toLowerCase();
-				$rootScope.username = $rootScope.user.fbUser.username ? $rootScope.user.fbUser.username : $rootScope.firstname + $rootScope.lastname;
+				if (!angular.equals(u, $rootScope.user)) {
+					$rootScope.user = u;
+					$rootScope.firstname = $rootScope.user.fbUser.first_name.toLowerCase();
+					$rootScope.lastname = $rootScope.user.fbUser.last_name.toLowerCase();
+					$rootScope.username = $rootScope.user.fbUser.username ? $rootScope.user.fbUser.username : $rootScope.firstname + $rootScope.lastname;
+				}
 			})
 			.error(function(e) {
 				console.log(e);
